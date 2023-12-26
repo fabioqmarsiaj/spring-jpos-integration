c303f65eaa875bbb60eda95a91b798e86c805c8e
Fabio Marsiaj <fabiobackend@gmail.com>
2023-12-26 13:26:08 -0300

    Simulating a basic flow with jpos local server

--- src/main/java/com/fabioqmarsiaj/springjposintegration/APIController.java
+++ src/main/java/com/fabioqmarsiaj/springjposintegration/APIController.java
@@ -1,21 +1,42 @@
 package com.fabioqmarsiaj.springjposintegration;
 
-import org.jpos.iso.ISOException;
-import org.jpos.iso.ISOMsg;
-import org.jpos.iso.MUX;
+import org.jpos.iso.*;
+import org.jpos.iso.channel.BASE24TCPChannel;
+import org.jpos.iso.channel.XMLChannel;
+import org.jpos.iso.header.BASE1Header;
+import org.jpos.iso.packager.Base1Packager;
 import org.jpos.q2.iso.QMUX;
+import org.jpos.q2.iso.QServer;
 import org.jpos.util.NameRegistrar;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class APIController {
 
     @GetMapping("")
-    public boolean checkIfMuxIsConnected() throws NameRegistrar.NotFoundException {
-        MUX mux = QMUX.getMUX("local_mux");
-        return mux.isConnected();
+    public void sendAndReceiveIsoMsg() throws IOException, ISOException {
+        ISOPackager packager = new CustomPackager();
+        ISOChannel channel = new BASE24TCPChannel("127.0.0.1", 10000, packager);
+
+        channel.connect();
+
+        ISOMsg request = new ISOMsg();
+
+        request.set(0, "0100");
+        request.set(1, "0000000000000000");
+
+        System.out.println("CHANNEL CONNECTION: " + channel.isConnected());
+
+        channel.send(request);
+        System.out.println("REQUEST: " + request);
+
+        ISOMsg response = channel.receive();
+        System.out.println("RESPONSE " + response);
+
     }
 }
---
+++ src/main/java/com/fabioqmarsiaj/springjposintegration/CustomPackager.java
@@ -0,0 +1,17 @@
+package com.fabioqmarsiaj.springjposintegration;
+
+import org.jpos.iso.IFB_NUMERIC;
+import org.jpos.iso.ISOBasePackager;
+import org.jpos.iso.ISOFieldPackager;
+
+public class CustomPackager extends ISOBasePackager {
+	protected ISOFieldPackager[] fields = {
+			/*000*/ new IFB_NUMERIC(4, "Message Type Indicator", false),
+			/*001*/ new IFB_NUMERIC(16, "Credit Card Number", false),
+	};
+
+	public CustomPackager() {
+		super();
+		setFieldPackager(fields);
+	}
+}
