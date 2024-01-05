d1df83f5d06bda0745afd81d69a3dd1924e8ce93
Fabio Marsiaj <fabiobackend@gmail.com>
2024-01-05 18:43:50 -0300

    To be continue: get the ISOMsg from the channel; why the return of mux.request is null?

--- src/main/java/com/fabioqmarsiaj/springjposintegration/APIController.java
+++ src/main/java/com/fabioqmarsiaj/springjposintegration/APIController.java
@@ -1,12 +1,9 @@
 package com.fabioqmarsiaj.springjposintegration;
 
-import org.jpos.iso.*;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOMsg;
 import org.jpos.iso.channel.BASE24TCPChannel;
-import org.jpos.iso.channel.XMLChannel;
-import org.jpos.iso.header.BASE1Header;
-import org.jpos.iso.packager.Base1Packager;
 import org.jpos.q2.iso.QMUX;
-import org.jpos.q2.iso.QServer;
 import org.jpos.util.NameRegistrar;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,25 +15,19 @@ import java.io.IOException;
 @RequestMapping("/api")
 public class APIController {
 
-    @GetMapping("")
-    public void sendAndReceiveIsoMsg() throws IOException, ISOException {
-        ISOPackager packager = new CustomPackager();
-        ISOChannel channel = new BASE24TCPChannel("127.0.0.1", 10000, packager);
+	@GetMapping("")
+	public void sendAndReceiveIsoMsg() throws ISOException, NameRegistrar.NotFoundException, IOException {
 
-        channel.connect();
+		QMUX mux = NameRegistrar.get("mux.mymux");
+		BASE24TCPChannel channelAdaptor = NameRegistrar.get("channel.mychannel");
 
-        ISOMsg request = new ISOMsg();
+		ISOMsg request = new ISOMsg();
 
-        request.set(0, "0100");
-        request.set(1, "0000000000000000");
+		request.set(0, "0100");
+		request.set(1, "0000000000000000");
 
-        System.out.println("CHANNEL CONNECTION: " + channel.isConnected());
-
-        channel.send(request);
-        System.out.println("REQUEST: " + request);
-
-        ISOMsg response = channel.receive();
-        System.out.println("RESPONSE " + response);
-
-    }
+		ISOMsg response = mux.request(request, 2000);
+//		ISOMsg response2 = channelAdaptor.receive();
+		System.out.println(response);
+	}
 }
